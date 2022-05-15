20c533dde8f86691be6e4700a2e601af9a1724c4
CarterPlus <hashcat@126.com>
2022-05-15 21:17:43 +0800

    feat(scrape-url): 完成入门第一个代码

--- scrape_url/src/main.rs
+++ scrape_url/src/main.rs
@@ -1,3 +1,15 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let url = "https://www.rust-lang.org/";
+    let output = "rust.md";
+
+    println!("Fetching url: {}", url);
+    let body: String = reqwest::blocking::get(url).unwrap().text().unwrap();
+
+    println!("Converting html to markdown...");
+    let md: String = html2md::parse_html(&body);
+
+    fs::write(output, md.as_bytes()).unwrap();
+    println!("Converted markdown has been saved in {}.", output);
 }
